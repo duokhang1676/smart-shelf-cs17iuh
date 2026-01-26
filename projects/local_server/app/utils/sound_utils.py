2e2cd48210b3dc1b97ec3159c649fad300b6a163
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2026-01-27 03:13:41 +0700

    update base dir

--- projects/local_server/app/utils/sound_utils.py
+++ projects/local_server/app/utils/sound_utils.py
@@ -32,7 +32,7 @@ from gtts import gTTS
 import subprocess
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../"))
 SOUND_PATH = os.path.join(BASE_DIR, "app/static/sounds/temp.mp3")
 
 def speech_text(text):
