8dead7beca4d3678789e531524cce1020a936f68
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2026-01-28 00:32:56 +0700

    fix sound utils

--- projects/local_server/app/utils/sound_utils.py
+++ projects/local_server/app/utils/sound_utils.py
@@ -40,14 +40,14 @@ def speech_text(text):
     tts.save(SOUND_PATH)
 
     subprocess.run(
-        ["mpg123", "-q", "-0", "alsa", SOUND_PATH],
+        ["mpg123", "-q", "-a", "hw:Device,0", SOUND_PATH],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
 
 def play_sound(path):
     subprocess.run(
-        ["mpg123", "-q", "-0", "alsa", path],
+        ["mpg123", "-q", "-a", "hw:Device,0", path],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
